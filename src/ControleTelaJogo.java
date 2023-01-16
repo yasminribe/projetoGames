c206a6b47c97eae985aea2e0ab56fe8081ab3a14
Lise Fliess <lisefliess@gmail.com>
2023-01-16 16:18:37 -0300

    controle de tela

--- src/ControleTelaJogo.java
+++ src/ControleTelaJogo.java
@@ -5,13 +5,39 @@ import java.awt.event.KeyListener;
 public class ControleTelaJogo implements KeyListener {
     private TelaJogo tela;
 
+    private  boolean cima;
+    private boolean baixo;
+    private boolean esquerda;
+    private boolean direita;
+
+    private valatile boolean pausado;
+
+
     public ControleTelaJogo(TelaJogo tela) {
         super();
         this.tela = tela;
+
+        cima = false;
+        baixo = false;
+        esquerda = false;
+        direita = true;
+        pausado = true;
+
+
     }
 
 // pressiona tecla
     @Override
+
+    public void run(){
+        while  (!pausado){
+            thread.sleep(10000/5);
+        }
+    }
+
+
+
+
     public void keyPressed(KeyEvent e) {
 
     }
