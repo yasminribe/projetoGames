1a077152854c313b12e309e5a5df13137505899a
Lise Fliess <lisefliess@gmail.com>
2023-01-16 14:06:24 -0300

    ControleTelaMenu

--- src/ControleTelaMenu.java
+++ src/ControleTelaMenu.java
@@ -1,25 +1,27 @@
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-    public class ControleTelaMenu implements ActionListener {
-        private static TelaMenu tela;
-        public ControleTelaMenu(TelaMenu tela){
-            this.tela = tela;
-        }
+public class ControleTelaMenu implements ActionListener {
+    private static TelaMenu tela;
 
-        public static void mostrarTelaMenu(){
-            tela.setVisible(true);
-        }
+    public ControleTelaMenu(TelaMenu tela) {
+        ControleTelaMenu.tela = tela;
+    }
 
-        @Override
-        public void actionPerformed(ActionEvent e) {
-            if(e.getSource()==tela.getNovoJogo()) {
+    public static void mostraTelaMenu() {
+        tela.setVisible(true);
+    }
 
-            }else if (e.getSource()==tela.getInstrucoes()){
-
-            }else if (e.getSource()==tela.getSair()){
-                System.exit(0);
-            }
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if (e.getSource() == tela.getNovoJogo()) {
+            new TelaJogo();
+            tela.setVisible(false);
+        } else if (e.getSource() == tela.getInstrucoes()) {
+            new TelaInstrucoes();
+            tela.setVisible(false);
+        } else if (e.getSource() == tela.getSair()) {
+            System.exit(0);
         }
     }
 }
