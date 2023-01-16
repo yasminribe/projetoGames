e7c11b92472f29768202157b661623a3eada5576
Yasmin <yasmin.5126c@gmail.com>
2023-01-16 14:23:22 -0300

    elementos que vão se apresentar na tela

---
+++ src/Elemento.java
@@ -0,0 +1,54 @@
+import java.awt.*;
+
+public abstract class Elemento {
+    private int x;
+    private int y;
+    private int largura;
+    private int altura;
+
+    public Elemento(int x, int y, int largura, int altura) {
+        this.x = x;
+        this.y = y;
+        this.largura = largura;
+        this.altura = altura;
+    }
+    public abstract void desenhar(Graphics g);
+
+    public void mover(int x , int y){
+        this.x=x;
+        this.y=y;
+
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public int getLargura() {
+        return largura;
+    }
+
+    public void setLargura(int largura) {
+        this.largura = largura;
+    }
+
+    public int getAltura() {
+        return altura;
+    }
+
+    public void setAltura(int altura) {
+        this.altura = altura;
+    }
+}
