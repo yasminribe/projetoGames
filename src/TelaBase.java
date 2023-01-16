ce55969a3f5ef9264460d918547bf899b77995a7
Yasmin <yasmin.5126c@gmail.com>
2023-01-16 14:10:50 -0300

    tela base para as outras class extender

--- src/TelaBase.java
+++ src/TelaBase.java
@@ -1,16 +1,15 @@
 import javax.swing.*;
-import java.io.Serial;
+import java.awt.*;
 
 public class TelaBase extends JFrame {
-    @Serial
-    private static final long serialVersionUID = 1L;
-    public TelaBase() {
-        setTitle("Snack Game");
-        setSize(600,400); // tamanho da tela
+    public TelaBase() throws HeadlessException {
+        setTitle("Snake Game");
+        setSize(640,400);
         setDefaultCloseOperation(DISPOSE_ON_CLOSE);
         setLocationRelativeTo(null);
         setLayout(null);
-        setResizable(false); //altera tamanho da tela
-        setIconImage(new ImageIcon("res/imgs/icon.png").getImage());
+        setResizable(false);
+        setIconImage(new ImageIcon("res/img/icon.png") {
+        }.getImage());
     }
 }
